e9c11b219141d5315fbc5252785d3e1d2644a4b3
blAs1N <qazasa123@gmail.com>
2020-05-25 08:39:05 +0900

    Build buff storage.

---
+++ Source/ProjectR/Public/BuffStorage.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BuffStorage.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class PROJECTR_API UBuffStorage : public UObject
+{
+	GENERATED_BODY()
+};
