924e1ded7cf5516cc0f4825e93c544f4fa9bf9c5
blAs1N <qazasa123@gmail.com>
2020-05-24 20:13:42 +0900

    Port player character.

---
+++ Source/ProjectR/Public/PlayerCharacter.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectRCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS(BlueprintType)
+class PROJECTR_API APlayerCharacter final : public AProjectRCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	APlayerCharacter();
+
+	void EquipWeapon(const FName& Name, uint8 Index);
+	int32 HealEnergy(int32 Value);
+
+	FORCEINLINE int32 GetEnergy() const noexcept { return Energy; }
+	FORCEINLINE int32 GetMaxEnergy() const noexcept { return MaxEnergy; }
+	FORCEINLINE int32 GetEnergyHeal() const noexcept { return EnergyHeal; }
+
+private:
+	void BeginPlay() override;
+	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	void SwapWeapon(uint8 Index);
+	void SwapWeapon(float Value);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, EditFixedSize, Category = Weapon, meta = (AllowPrivateAccess = true))
+	TArray<class AWeapon*> Weapons;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Transient, Category = Stat, meta = (AllowPrivateAccess = true))
+	int32 Energy;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Transient, Category = Stat, meta = (AllowPrivateAccess = true))
+	int32 MaxEnergy;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Transient, Category = Stat, meta = (AllowPrivateAccess = true))
+	int32 EnergyHeal;
+
+	uint8 CurWeaponIndex;
+};
