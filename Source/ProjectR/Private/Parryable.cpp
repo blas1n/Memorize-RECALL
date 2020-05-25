abbbae0d2576e64080e58f16eb3a4ca6d6c93043
blAs1N <qazasa123@gmail.com>
2020-05-25 19:39:09 +0900

    Build 'IParryable' interface.

---
+++ Source/ProjectR/Private/Parryable.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Parryable.h"
+#include "GameFramework/Controller.h"
+
+//bool IParryable::IsParryable(float Damage, AController* Instigator, AActor* Causer) const
+//{
+//	return false;
+//}
+//
+//void IParryable::Parry(float Damage, AController* Instigator, AActor* Causer) {}
