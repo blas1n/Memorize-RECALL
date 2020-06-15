d4df825bc485b9a723e61a208035d2653011fba0
blAs1N <qazasa123@gmail.com>
2020-06-15 15:27:49 +0900

    Change weapon data structure
    Use animation layer. See my blog later.

---
+++ Source/ProjectR/Public/WeaponData.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "WeaponData.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FWeaponData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Key;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TAssetPtr<class UStaticMesh> RightMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform RightTransform;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TAssetPtr<UStaticMesh> LeftMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform LeftTransform;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<class ASkill>> Skills;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UAnimInstance* UpperAnimInstance;
+};
