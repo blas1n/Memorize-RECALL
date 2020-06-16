c0bf64df6947e33b7aef3e13ec4c0447925151ff
blAs1N <qazasa123@gmail.com>
2020-06-16 13:03:54 +0900

    Create game mode base

---
+++ Source/ProjectR/Public/ProjectRGameMode.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectRGameMode.generated.h"
+
+UCLASS()
+class PROJECTR_API AProjectRGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
