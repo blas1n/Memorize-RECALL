7f64c5713f42fe6736e3deb754d9e1c1223d797b
blAs1N <qazasa123@gmail.com>
2020-06-16 13:06:48 +0900

    Implement stat setter

--- Source/ProjectR/Private/ProjectRPlayerState.cpp
+++ Source/ProjectR/Private/ProjectRPlayerState.cpp
@@ -1,36 +1,68 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ProjectRPlayerState.h"
+#include "Engine/World.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "ProjectRGameState.h"
 
-void FStat::HealStat(uint8 Value) noexcept
+void AProjectRPlayerState::HealHealth(uint8 Value) noexcept
 {
-	CurStat += Value;
-	CurStat = FMath::Clamp(CurStat, static_cast<uint8>(0u), MaxStat);
+	Health += Value;
+	Health = FMath::Clamp(Health, static_cast<uint8>(0u), MaxHealth);
 }
 
-void FStat::HealStatByDamage(uint8 Damage) noexcept
+void AProjectRPlayerState::HealHealthByDamage(uint8 Damage) noexcept
 {
-	if (Damage > 0.0f && StatHeal != 0.0f)
-		HealStat(static_cast<float>(Damage) * StatHeal);
+	if (Damage > 0.0f && HealthHeal != 0.0f)
+		HealHealth(static_cast<float>(Damage) * HealthHeal);
 }
 
-void FStat::SetMaxStat(uint8 Value, bool bWithCur) noexcept
+void AProjectRPlayerState::SetMaxHealth(uint8 Value, bool bWithCur) noexcept
 {
-	MaxStat += Value;
-
-	if (bWithCur)
-		CurStat += Value;
+	MaxHealth += Value;
+	if (bWithCur) Health += Value;
 }
 
-void FStat::SetStatHeal(float Value) noexcept
+void AProjectRPlayerState::SetHealthHeal(float Value) noexcept
 {
-	StatHeal = Value;
+	HealthHeal = Value;
+}
+
+void AProjectRPlayerState::HealEnergy(uint8 Value) noexcept
+{
+	Energy += Value;
+	Energy = FMath::Clamp(Energy, static_cast<uint8>(0u), MaxEnergy);
+}
+
+void AProjectRPlayerState::HealEnergyByDamage(uint8 Damage) noexcept
+{
+	if (Damage > 0.0f && EnergyHeal != 0.0f)
+		HealEnergy(static_cast<float>(Damage) * EnergyHeal);
+}
+
+void AProjectRPlayerState::SetMaxEnergy(uint8 Value, bool bWithCur) noexcept
+{
+	MaxEnergy += Value;
+	if (bWithCur) Energy += Value;
+}
+
+void AProjectRPlayerState::SetEnergyHeal(float Value) noexcept
+{
+	EnergyHeal = Value;
 }
 
 void AProjectRPlayerState::SetCrouchSpeed(float Value) noexcept
 {
 	GetPawn<ACharacter>()->GetCharacterMovement()
 		->MaxWalkSpeedCrouched = CrouchSpeed = Value;
+}
+
+void AProjectRPlayerState::BeginPlay()
+{
+	auto* GameState = Cast<AProjectRGameState>
+		(UGameplayStatics::GetGameState(GetWorld()));
+
+	GameState->InitBuffStorages(BuffStorages);
 }
