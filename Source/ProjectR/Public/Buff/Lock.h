e75100b6dd2e4fef0b9d3e2d09473ec352572d28
blAs1N <qazasa123@gmail.com>
2020-07-03 10:21:55 +0900

    Build lock buff

---
+++ Source/ProjectR/Public/Buff/Lock.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Buff/Buff.h"
+#include "Lock.generated.h"
+
+UCLASS()
+class PROJECTR_API ULock final : public UBuff
+{
+	GENERATED_BODY()
+
+public:
+	FORCEINLINE void SetLockedTarget(class AProjectRCharacter*
+		InLockedTarget) noexcept { LockedTarget = InLockedTarget; }
+	
+	FORCEINLINE AProjectRCharacter* GetLockedTarget()
+		const noexcept { return LockedTarget; }
+
+private:
+	void Tick(float DeltaSeconds) override;
+
+	void BeginBuff() override;
+	void EndBuff() override;
+
+	bool IsActivate_Implementation() const override;
+
+private:
+	UPROPERTY()
+	AProjectRCharacter* LockedTarget;
+
+	uint8 bIsLocked : 1;
+};
