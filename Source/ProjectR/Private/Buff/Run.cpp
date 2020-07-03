c7fedc052132ace880ff6ba7aaee1c933e13f190
blAs1N <qazasa123@gmail.com>
2020-07-03 10:18:30 +0900

    Build run buff class

---
+++ Source/ProjectR/Private/Buff/Run.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Buff/Run.h"
+#include "Character/ProjectRCharacter.h"
+
+void URun::BeginBuff()
+{
+	if (++Count > 1)
+		return;
+
+	GetTarget()->Walk();
+}
+
+void URun::EndBuff()
+{
+	--Count;
+}
+
+bool URun::IsActivate_Implementation() const
+{
+	return Count > 0;
+}
