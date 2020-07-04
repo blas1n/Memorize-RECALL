f46014d70d635864c50f10c26c229a9696079689
blAs1N <qazasa123@gmail.com>
2020-07-04 11:41:47 +0900

    Create ai controller

---
+++ Source/ProjectR/Public/Character/ProjectRAIController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ProjectRAIController.generated.h"
+
+UCLASS()
+class PROJECTR_API AProjectRAIController final : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AProjectRAIController();
+};
