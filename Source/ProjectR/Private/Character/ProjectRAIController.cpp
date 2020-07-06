305256d42f1731f7bab8f063edda0fe5f83c63b3
blAs1N <qazasa123@gmail.com>
2020-07-06 15:56:42 +0900

    Use control desired movement

--- Source/ProjectR/Private/Character/ProjectRAIController.cpp
+++ Source/ProjectR/Private/Character/ProjectRAIController.cpp
@@ -10,37 +10,3 @@ AProjectRAIController::AProjectRAIController()
 {
 	bWantsPlayerState = true;
 }
-
-void AProjectRAIController::OnPossess(APawn* InPawn)
-{
-	Super::OnPossess(InPawn);
-	User = Cast<AProjectRCharacter>(InPawn);
-}
-
-void AProjectRAIController::OnUnPossess()
-{
-	User = nullptr;
-	Super::OnUnPossess();
-}
-
-void AProjectRAIController::Tick(float DeltaSeconds)
-{
-	Super::Tick(DeltaSeconds);
-
-	if (!User) return;
-
-	FVector Target;
-	if (!GetMoveTargetLocation(Target)) return;
-
-	const FRotator Look = UKismetMathLibrary::FindLookAtRotation(User->GetActorLocation(), Target);
-	User->SetTurn(Look.Yaw);
-}
-
-bool AProjectRAIController::GetMoveTargetLocation(FVector& Out) const
-{
-	auto* MyBlackbaord = GetBlackboardComponent();
-	if (!MyBlackbaord) return false;
-
-	Out = MyBlackbaord->GetValueAsVector(TEXT("TargetLocation"));
-	return true;
-}
