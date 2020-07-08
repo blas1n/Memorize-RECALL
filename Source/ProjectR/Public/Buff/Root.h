8a2a554b26a2e1969245591f71d51e4215157cb1
blAs1N <qazasa123@gmail.com>
2020-07-08 14:39:55 +0900

    Recreate root buff

---
+++ Source/ProjectR/Public/Buff/Root.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Buff/Buff.h"
+#include "Root.generated.h"
+
+UCLASS(BlueprintType)
+class PROJECTR_API URoot : public UBuff
+{
+	GENERATED_BODY()
+	
+private:
+	void OnApply() override;
+	void OnRelease() override;
+
+	bool IsActivate_Implementation() const override;
+
+private:
+	uint8 Count;
+};
