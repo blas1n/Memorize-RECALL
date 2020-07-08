f5901dfc039b40ba73eef7f286e252becffc1699
blAs1N <qazasa123@gmail.com>
2020-07-08 13:26:58 +0900

    Create cast buff

--- Source/ProjectR/Private/Buff/Cast.cpp
+++ Source/ProjectR/Private/Buff/Cast.cpp
@@ -1,6 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Buff/Cast.h"
+#include "Skill.h"
+
+void UCast::StopSkill()
+{
+	if (!IsActivate()) return;
+	
+	CurSkill->End();
+	CurSkill = nullptr;
+}
+
+void UCast::SetCurSkill(USkill* Skill)
+{
+	if (!CanUseSkill(Skill)) return;
+
+	StopSkill();
+	CurSkill = Skill;
+}
 
 bool UCast::CanUseSkill(const USkill* Skill) const
 {
