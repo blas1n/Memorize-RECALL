85d3427024c8f6af12e8522a14b3976a003949bb
blAs1N <qazasa123@gmail.com>
2020-07-09 23:01:23 +0900

    Build attach parent enum

---
+++ Source/ProjectR/Public/Data/AttachParent.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "AttachParent.generated.h"
+
+UENUM(BlueprintType)
+enum class EAttachParent : uint8
+{
+	None, Mesh, RightWeapon, LeftWeapon,
+};
