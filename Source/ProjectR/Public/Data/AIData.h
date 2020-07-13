a1a1953fb5a7a74f63f0c7149c7b0b93306ea6e7
blAs1N <qazasa123@gmail.com>
2020-07-13 20:32:28 +0900

    Port struct

---
+++ Source/ProjectR/Public/Data/AIData.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "Templates/SubclassOf.h"
+#include "LogicData.h"
+#include "AIData.generated.h"
+
+USTRUCT(Atomic, BlueprintType)
+struct FAIData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Key;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FName> WeaponNames;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TAssetPtr<class USkeletalMesh> Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<class UAnimInstance> AnimClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLogicData LogicData;
+};
