072eb393de3f8646cb37805a38a47a3c53ce11b7
blAs1N <qazasa123@gmail.com>
2020-07-13 20:33:11 +0900

    Build perception system

--- Source/ProjectR/Public/Character/ProjectRAIController.h
+++ Source/ProjectR/Public/Character/ProjectRAIController.h
@@ -4,8 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
-#include "Data/PerceptionData.h"
 #include "ProjectRAIController.generated.h"
+
+UENUM(BlueprintType)
+enum class EAIState : uint8
+{
+	Patrol, Detection, Chase, Quest,
+};
 
 UCLASS()
 class PROJECTR_API AProjectRAIController final : public AAIController
@@ -15,18 +20,41 @@ class PROJECTR_API AProjectRAIController final : public AAIController
 public:
 	AProjectRAIController();
 
-protected:
-	UFUNCTION(BlueprintCallable, meta = (BlueprintProtected))
-	void InitPerception(const struct FPerceptionData& Data);
+	void InitLogic(const struct FLogicData& LogicData);
 
 private:
+	void Tick(float DeltaSeconds) override;
+
 	void OnPossess(APawn* InPawn) override;
 	void OnUnPossess() override;
+
+	void InitBlackboard(const FLogicData& LogicData);
+
+	UFUNCTION()
+	void OnPerceptionUpdated(const TArray<AActor*>& UpdatedActors);
+
+	void OnSightUpdated(const AActor* Actor, const struct FAIStimulus& Stimulus);
+	void SetFloorLocation(const FVector& BaseLocation);
 
 	UFUNCTION()
 	void OnDeath(AController* LastInstigator);
 
+	void SetAIState(EAIState NewAIState);
+
 private:
 	UPROPERTY()
 	class UAISenseConfig_Sight* SightConfig;
+
+	UPROPERTY()
+	const AActor* TargetActor;
+
+	FTimerHandle QuestHandle;
+	float QuestDuration;
+	
+	float DetectionValue;
+	float DetectionIncrease;
+	float DetectionDecrease;
+
+	EAIState AIState;
+	uint8 bIsSeePlayer : 1;
 };
