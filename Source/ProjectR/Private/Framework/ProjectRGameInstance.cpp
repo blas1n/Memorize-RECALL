c1d69fb11acb33ee0af1a6bad311e2b4ad198872
blAs1N <qazasa123@gmail.com>
2020-07-16 22:53:34 +0900

    Remove data table property in game instance

--- Source/ProjectR/Private/Framework/ProjectRGameInstance.cpp
+++ Source/ProjectR/Private/Framework/ProjectRGameInstance.cpp
@@ -1,9 +1,3 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Framework/ProjectRGameInstance.h"
-
-UDataTable* UProjectRGameInstance::GetDataTable(FName Name) const
-{
-	auto Table = DataTables.Find(Name);
-	return Table ? *Table : nullptr;
-}
