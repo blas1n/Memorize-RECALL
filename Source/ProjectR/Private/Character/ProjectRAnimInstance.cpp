9e0e7d3c4bc0b543f454cf3d439cdb85b9b63f0d
blAs1N <qazasa123@gmail.com>
2020-07-18 17:29:57 +0900

    Remove unused notify

--- Source/ProjectR/Private/Character/ProjectRAnimInstance.cpp
+++ Source/ProjectR/Private/Character/ProjectRAnimInstance.cpp
@@ -2,14 +2,10 @@
 
 #include "Character/ProjectRAnimInstance.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "Kismet/KismetMathLibrary.h"
 #include "Buff/Stun.h"
 #include "Buff/Lock.h"
-#include "Buff/Root.h"
 #include "Character/ProjectRCharacter.h"
 #include "BuffLibrary.h"
-#include "Weapon.h"
-#include "WeaponComponent.h"
 
 UProjectRAnimInstance::UProjectRAnimInstance()
 	: Super()
@@ -34,27 +30,4 @@ void UProjectRAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 	bIsLooking = UBuffLibrary::IsActivate<ULock>(User);
 	bIsStuned = UBuffLibrary::IsActivate<UStun>(User);
 	bIsInAir = Movement->IsFalling();
-}
-
-void UProjectRAnimInstance::AnimNotify_Execute()
-{
-	const auto* User = Cast<AProjectRCharacter>(TryGetPawnOwner());
-	if (!User) return;
-
-	auto* Weapon = User->GetWeaponComponent()->GetWeapon();
-	if (!Weapon) return;
-
-	Weapon->OnExecute.Broadcast();
-}
-
-void UProjectRAnimInstance::AnimNotify_EnableMove()
-{
-	if (auto* User = Cast<AProjectRCharacter>(TryGetPawnOwner()))
-		UBuffLibrary::ReleaseBuff<URoot>(User);
-}
-
-void UProjectRAnimInstance::AnimNotify_DisableMove()
-{
-	if (auto* User = Cast<AProjectRCharacter>(TryGetPawnOwner()))
-		UBuffLibrary::ApplyBuff<URoot>(User);
 }
