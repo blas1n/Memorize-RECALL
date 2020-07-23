b3e28fbcd026aa1ded0ce5bc93c31665fbd865c3
blAs1N <qazasa123@gmail.com>
2020-07-23 11:06:06 +0900

    Clear code of ai controller

--- Source/ProjectR/Private/Character/ProjectRAIController.cpp
+++ Source/ProjectR/Private/Character/ProjectRAIController.cpp
@@ -56,50 +56,10 @@ void AProjectRAIController::InitLogic(const TAssetPtr<UBehaviorTree>& BehaviorTr
 	GetPerceptionComponent()->RequestStimuliListenerUpdate();
 }
 
-void AProjectRAIController::SetAIState(EAIState NewAIState)
-{
-	AIState = NewAIState;
-	if (auto* MyBlackboard = GetBlackboardComponent())
-		MyBlackboard->SetValueAsEnum(TEXT("AIState"), static_cast<uint8>(AIState));
-
-	DetectionValue = 0.0f;
-
-	auto* MyPawn = GetPawn<AProjectRCharacter>();
-	UBuffLibrary::ReleaseBuff<URun>(MyPawn);
-
-	if (AIState == EAIState::Patrol)
-		UBuffLibrary::ReleaseBuff<ULock>(MyPawn);
-	else
-		UBuffLibrary::ApplyBuff<ULock>(MyPawn);
-}
-
 void AProjectRAIController::BeginPlay()
 {
 	Super::BeginPlay();
 	GetPerceptionComponent()->OnTargetPerceptionUpdated.AddDynamic(this, &AProjectRAIController::OnPerceptionUpdated);
-}
-
-void AProjectRAIController::Tick(float DeltaSeconds)
-{
-	Super::Tick(DeltaSeconds);
-
-	if (TargetActor)
-	{
-		if (Cast<AProjectRCharacter>(TargetActor)->IsDeath())
-		{
-			SetAIState(EAIState::Patrol);
-			TargetActor = nullptr;
-		}
-		else
-		{
-			SetFloorLocation(TargetActor->GetActorLocation());
-		}
-	}
-
-	if (AIState == EAIState::Detection)
-		UpdateDetection(DeltaSeconds);
-	else if (AIState == EAIState::Chase)
-		UpdateChase();
 }
 
 void AProjectRAIController::OnPossess(APawn* InPawn)
@@ -131,99 +91,9 @@ void AProjectRAIController::InitBlackboard(const FLogicData& LogicData)
 	}
 }
 
-void AProjectRAIController::UpdateDetection(float DeltaSeconds)
-{
-	auto* MyPawn = GetPawn<AProjectRCharacter>();
-	if (MyPawn || !bIsSeePlayer)
-		return;
-
-	const FVector PawnLocation = MyPawn->GetActorLocation();
-	const FVector TargetLocation = TargetActor->GetActorLocation();
-	const float Distance = (PawnLocation - TargetLocation).Size();
-
-	if (Distance <= ImmediateDetectionRadius)
-	{
-		SetAIState(EAIState::Chase);
-		return;
-	}
-
-	const float CurveValue = FMath::GetMappedRangeValueUnclamped(
-		FVector2D{ ImmediateDetectionRadius, SightRadius },
-		FVector2D{ 0.0f, 1.0f },
-		Distance
-	);
-
-	DetectionValue += DetectionCurve->GetFloatValue(CurveValue) * DeltaSeconds;
-	if (DetectionValue >= 1.0f)
-		SetAIState(EAIState::Chase);
-}
-
-void AProjectRAIController::UpdateChase()
-{
-	auto* MyPawn = GetPawn<AProjectRCharacter>();
-
-	if (!TargetActor)
-	{
-		UBuffLibrary::ReleaseBuff<ULock>(MyPawn);
-		UBuffLibrary::ApplyBuff<URun>(MyPawn);
-	}
-	else
-	{
-		const FVector PawnLocation = MyPawn->GetActorLocation();
-		const FVector TargetLocation = TargetActor->GetActorLocation();
-		const float DistanceSquared = (PawnLocation - TargetLocation).SizeSquared();
-
-		if (DistanceSquared <= LockRadiusSquared)
-		{
-			UBuffLibrary::ReleaseBuff<URun>(MyPawn);
-			UBuffLibrary::ApplyBuff<ULock>(MyPawn);
-		}
-		else
-		{
-			UBuffLibrary::ReleaseBuff<ULock>(MyPawn);
-			UBuffLibrary::ApplyBuff<URun>(MyPawn);
-		}
-	}
-}
-
 void AProjectRAIController::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	if (Stimulus.Type == UAISense::GetSenseID<UAISense_Sight>())
-		OnSightUpdated(Actor, Stimulus);
-}
 
-void AProjectRAIController::OnSightUpdated(AActor* Actor, const FAIStimulus& Stimulus)
-{
-	if (Cast<AProjectRCharacter>(Actor)->IsDeath())
-		return;
-
-	bIsSeePlayer = Stimulus.IsActive();
-	if (auto* MyBlackboard = GetBlackboardComponent())
-		MyBlackboard->SetValueAsBool(TEXT("IsSeePlayer"), bIsSeePlayer);
-
-	if (bIsSeePlayer)
-	{
-		TargetActor = Actor;
-		if (AIState == EAIState::Patrol)
-			SetAIState(EAIState::Detection);
-	}
-	else TargetActor = nullptr;
-
-	if (auto* Lock = UBuffLibrary::GetBuff<ULock>(GetPawn<AProjectRCharacter>()))
-		Lock->SetLockTarget(TargetActor);
-}
-
-void AProjectRAIController::SetFloorLocation(const FVector& BaseLocation)
-{
-	if (auto* MyBlackboard = GetBlackboardComponent())
-	{
-		FVector End = BaseLocation;
-		End.Z = -1000.0f;
-
-		FHitResult Hit;
-		GetWorld()->LineTraceSingleByObjectType(Hit, BaseLocation, End, { ECollisionChannel::ECC_WorldStatic });
-		MyBlackboard->SetValueAsVector(TEXT("TargetLocation"), Hit.ImpactPoint);
-	}
 }
 
 void AProjectRAIController::OnDeath(AController* LastInstigator)
