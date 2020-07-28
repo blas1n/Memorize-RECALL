113bed2d39d92a76f09d768d86db822587724222
blAs1N <qazasa123@gmail.com>
2020-07-28 23:06:13 +0900

    Call delegate when real active and deactive

--- Source/ProjectR/Private/Buff/Buff.cpp
+++ Source/ProjectR/Private/Buff/Buff.cpp
@@ -25,10 +25,13 @@ void UBuff::Apply()
 	bIsActivateWithoutBlock = true;
 	if (IsBlocked()) return;
 
+	const bool bIsAlreadActive = IsActivate();
+
 	OnApply();
 	RecieveOnApply();
 
-	OnApplied.Broadcast();
+	if (!bIsAlreadActive && IsActivate())
+		OnApplied.Broadcast();
 }
 
 void UBuff::Release()
@@ -36,10 +39,13 @@ void UBuff::Release()
 	bIsActivateWithoutBlock = false;
 	if (IsBlocked()) return;
 
+	const bool bIsAlreadActive = IsActivate();
+
 	OnRelease();
 	RecieveOnRelease();
 
-	OnReleased.Broadcast();
+	if (bIsAlreadActive && !IsActivate())
+		OnReleased.Broadcast();
 }
 
 void UBuff::Block()
