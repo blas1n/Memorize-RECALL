1e6abed74ef37bdd16407791497779d609cacd5d
blAs1N <qazasa123@gmail.com>
2020-08-09 22:52:27 +0900

    Fix wrong file name

--- Source/ProjectR/Private/Notify/AnimNotifyState_Combo.cpp
+++ Source/ProjectR/Private/Notify/AnimNotifyState_Combo.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Notify/AnimNotify_Combo.h"
+#include "Notify/AnimNotifyState_Combo.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Component/WeaponComponent.h"
 #include "Framework/PRCharacter.h"
