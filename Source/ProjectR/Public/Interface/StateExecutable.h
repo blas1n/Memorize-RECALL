5db41e71c03e3aff07801c1c67e82e5a27b3c8a2
blAs1N <qazasa123@gmail.com>
2020-08-10 15:14:27 +0900

    Create executable interface

---
+++ Source/ProjectR/Public/Interface/StateExecutable.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "StateExecutable.generated.h"
+
+UINTERFACE(BlueprintType, MinimalAPI)
+class UStateExecutable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class PROJECTR_API IStateExecutable
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void BeginExecute();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void EndExecute();
+};
