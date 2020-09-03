470157724bbf960b8e01ffcbc9c204706aae0c52
blAs1N <qazasa123@gmail.com>
2020-09-03 14:11:26 +0900

    Create anim data

---
+++ Source/ProjectR/Public/Data/AnimData.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "AnimData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAnimData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	class UBlendSpace1D* NotLock;
+
+	UPROPERTY(BlueprintReadWrite)
+	class UBlendSpace* Lock;
+
+	UPROPERTY(BlueprintReadWrite)
+	class UAnimSequence* Air;
+
+	UPROPERTY(BlueprintReadWrite)
+	UAnimSequence* AirEnd;
+};
