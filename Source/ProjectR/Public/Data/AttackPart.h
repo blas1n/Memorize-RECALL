c9d8225cf49365d59f7b57eeac420d098a3cb8a5
blAs1N <qazasa123@gmail.com>
2020-09-15 10:08:43 +0900

    Use unreal enum

--- Source/ProjectR/Public/Data/AttackPart.h
+++ Source/ProjectR/Public/Data/AttackPart.h
@@ -9,9 +9,8 @@
 UENUM(BlueprintType, meta = (Bitflags))
 enum EAttackPart // Can add other part. (Ex. Head)
 {
-	None = 0x00 UMETA(Hidden),
-	RightWeapon = 0x01 UMETA(DisplayName = "RightWeapon"),
-	LeftWeapon = 0x02 UMETA(DisplayName = "LeftWeapon"),
-	RightFoot = 0x04 UMETA(DisplayName = "RightFoot"),
-	LeftFoot = 0x08 UMETA(DisplayName = "LeftFoot"),
+	RightWeapon,
+	LeftWeapon,
+	RightFoot,
+	LeftFoot,
 };
